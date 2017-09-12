115a058348bdc0a2cc2a13b495b18e4d8f64afc1
mrawlingst <mrawlingst@gmail.com>
2017-09-11 22:18:22 -0400

    [A2] Completed the main function of assignment 2

--- Assignment2/app/src/main/java/edu/eku/mrawlings/assignment2/MainActivity.java
+++ Assignment2/app/src/main/java/edu/eku/mrawlings/assignment2/MainActivity.java
@@ -2,12 +2,127 @@ package edu.eku.mrawlings.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity
+{
+    private int judgeScores[];
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_calculate).setOnClickListener(calculateScore);
     }
+
+    private final View.OnClickListener calculateScore =
+            new View.OnClickListener()
+            {
+                public void onClick(View button)
+                {
+                    judgeScores = new int[7];
+
+                    // Judge 1
+                    EditText et_judge1 = (EditText)findViewById(R.id.judge1_score);
+
+                    try
+                    {
+                        int j1score = Integer.parseInt(et_judge1.getText().toString());
+                        if (j1score < 0)
+                            judgeScores[0] = 0;
+                        else if (j1score > 10)
+                            judgeScores[0] = 10;
+                        else
+                            judgeScores[0] = j1score;
+
+                        et_judge1.setText("" + judgeScores[0]);
+                    }
+                    catch(Exception e)
+                    {
+                        judgeScores[0] = 0;
+                    }
+
+                    // Judge 2
+                    SeekBar sb_judge2 = (SeekBar)findViewById(R.id.judge2_score);
+                    judgeScores[1] = sb_judge2.getProgress();
+
+                    // Judge 3
+                    Spinner sp_judge3 = (Spinner)findViewById(R.id.judge3_score);
+                    judgeScores[2] = Integer.parseInt(sp_judge3.getSelectedItem().toString());
+
+                    // Judge 3
+                    Spinner sp_judge4 = (Spinner)findViewById(R.id.judge4_score);
+                    judgeScores[3] = Integer.parseInt(sp_judge4.getSelectedItem().toString());
+
+                    // Judge 3
+                    Spinner sp_judge5 = (Spinner)findViewById(R.id.judge5_score);
+                    judgeScores[4] = Integer.parseInt(sp_judge5.getSelectedItem().toString());
+
+                    // Judge 3
+                    Spinner sp_judge6 = (Spinner)findViewById(R.id.judge6_score);
+                    judgeScores[5] = Integer.parseInt(sp_judge6.getSelectedItem().toString());
+
+                    // Judge 7
+                    Spinner sp_judge7 = (Spinner)findViewById(R.id.judge7_score);
+                    judgeScores[6] = Integer.parseInt(sp_judge7.getSelectedItem().toString());
+
+
+                    // Total Score
+                    int total_score = 0;
+                    bubbleSort(judgeScores);
+                    judgeScores = Arrays.copyOf(judgeScores, judgeScores.length - 1);
+                    reverseBubbleSort(judgeScores);
+                    judgeScores = Arrays.copyOf(judgeScores, judgeScores.length - 1);
+
+                    for (int i : judgeScores)
+                        total_score += i;
+                    total_score /= judgeScores.length;
+                    TextView tv_totalscore = (TextView)findViewById(R.id.total_score);
+                    tv_totalscore.setText("" + total_score);
+                }
+
+                private void bubbleSort(int[] input)
+                {
+                    int length = input.length;
+                    int temp = 0;
+                    for (int i = 0; i < length; i++)
+                    {
+                        for (int j = 1; j < (length - i); j++)
+                        {
+                            if (input[j-1] > input[j])
+                            {
+                                temp = input[j-1];
+                                input[j-1] = input[j];
+                                input[j] = temp;
+                            }
+                        }
+                    }
+                }
+
+                private void reverseBubbleSort(int[] input)
+                {
+                    int length = input.length;
+                    int temp = 0;
+                    for (int i = 0; i < length; i++)
+                    {
+                        for (int j = 1; j < (length - i); j++)
+                        {
+                            if (input[j-1] < input[j])
+                            {
+                                temp = input[j-1];
+                                input[j-1] = input[j];
+                                input[j] = temp;
+                            }
+                        }
+                    }
+                }
+            };
 }
