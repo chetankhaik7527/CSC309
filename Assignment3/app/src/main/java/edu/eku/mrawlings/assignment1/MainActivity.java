f944d6e5cba2352d972e3f758acf123c8c40d72c
mrawlingst <mrawlingst@gmail.com>
2017-09-14 22:28:14 -0400

    [A3] Heavy work of assignment completed

--- Assignment3/app/src/main/java/edu/eku/mrawlings/assignment1/MainActivity.java
+++ Assignment3/app/src/main/java/edu/eku/mrawlings/assignment1/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.eku.mrawlings.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    //
+    public static String EXTRA_LOWERTAXBRACKET = "lowertaxbracket";
+    public static String EXTRA_MIDDLETAXBRACKET = "middletaxbracket";
+    public static String EXTRA_TAXRATE1 = "taxrate1";
+    public static String EXTRA_TAXRATE2 = "taxrate2";
+    public static String EXTRA_TAXRATE3 = "taxrate3";
+
     // View objects
     private TextView taxDisplay;
+
+    //
+    private int taxLevel1 = 10000;
+    private int taxLevel2 = 100000;
+
+    //
+    private double taxRate1 = 0.7;
+    private double taxRate2 = 0.9;
+    private double taxRate3 = 1.1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +42,7 @@ public class MainActivity extends AppCompatActivity
 
         // Set button listener
         findViewById(R.id.btn_calculate).setOnClickListener(calculateIncome);
+        findViewById(R.id.btn_settings).setOnClickListener(goToSettings);
     }
 
     private final View.OnClickListener calculateIncome =
@@ -49,19 +67,19 @@ public class MainActivity extends AppCompatActivity
                     }
 
                     // Calculate tax for income below or equal to $10,000
-                    if (income <= 10000)
+                    if (income <= taxLevel1)
                     {
-                        tax = (income * .7);
+                        tax = taxRate1 * income;
                     }
                     // Calculate tax for income between $10,000 and $100,000
-                    else if (income <= 100000)
+                    else if (income <= taxLevel2)
                     {
-                        tax = ((income - 10000) * .9 + 7000);
+                        tax = taxRate1 * taxLevel1 + taxRate2 * (income - taxLevel1);
                     }
                     // Calculate tax for income above $100,000
-                    else if (income > 100000)
+                    else if (income > taxLevel2)
                     {
-                        tax = ((income - 100000) * 1.1 + 88000);
+                        tax = taxRate1 * taxLevel1 + taxRate2 * (taxLevel2 - taxLevel1) + taxRate3 * (income - taxLevel2);
                     }
                     // In case other cases fails
                     else
@@ -73,4 +91,35 @@ public class MainActivity extends AppCompatActivity
                     taxDisplay.setText("$" + String.format("%.2f", tax));
                 }
             };
+
+    private final View.OnClickListener goToSettings =
+            new View.OnClickListener()
+            {
+                public void onClick(View button)
+                {
+                    Intent settingsIntent = new Intent(getApplicationContext(), SettingsActivity.class);
+                    settingsIntent.putExtra(EXTRA_LOWERTAXBRACKET, taxLevel1);
+                    settingsIntent.putExtra(EXTRA_MIDDLETAXBRACKET, taxLevel2);
+                    settingsIntent.putExtra(EXTRA_TAXRATE1, taxRate1);
+                    settingsIntent.putExtra(EXTRA_TAXRATE2, taxRate2);
+                    settingsIntent.putExtra(EXTRA_TAXRATE3, taxRate3);
+                    startActivityForResult(settingsIntent, 123);
+                }
+            };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if (resultCode == RESULT_OK)
+        {
+            taxLevel1 = intent.getIntExtra(MainActivity.EXTRA_LOWERTAXBRACKET, 10000);
+            taxLevel2 = intent.getIntExtra(MainActivity.EXTRA_MIDDLETAXBRACKET, 100000);
+
+            taxRate1 = intent.getDoubleExtra(MainActivity.EXTRA_TAXRATE1, 70) / 100.0;
+            taxRate2 = intent.getDoubleExtra(MainActivity.EXTRA_TAXRATE2, 90) / 100.0;
+            taxRate3 = intent.getDoubleExtra(MainActivity.EXTRA_TAXRATE3, 110) / 100.0;
+
+            taxDisplay.setText("testing");
+        }
+    }
 }
---
+++ Assignment3/app/src/main/java/edu/eku/mrawlings/assignment1/SettingsActivity.java
@@ -0,0 +1,103 @@
+package edu.eku.mrawlings.assignment1;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SettingsActivity extends AppCompatActivity {
+    //
+    private int taxLevel1 = 10000;
+    private int taxLevel2 = 100000;
+
+    //
+    private double taxRate1 = 0.7;
+    private double taxRate2 = 0.9;
+    private double taxRate3 = 1.1;
+
+    // View objects
+    EditText et_LowerBracketTax;
+    EditText et_MiddleBracketTax;
+
+    EditText et_LowerBracketRate;
+    EditText et_MiddleBracketRate;
+    EditText et_UpperBracketRate;
+
+    // Error message
+    private String error_message = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Intent intent = getIntent();
+
+        taxLevel1 = intent.getIntExtra(MainActivity.EXTRA_LOWERTAXBRACKET, 10000);
+        taxLevel2 = intent.getIntExtra(MainActivity.EXTRA_MIDDLETAXBRACKET, 100000);
+
+        taxRate1 = intent.getDoubleExtra(MainActivity.EXTRA_TAXRATE1, 0.7);
+        taxRate2 = intent.getDoubleExtra(MainActivity.EXTRA_TAXRATE2, 0.9);
+        taxRate3 = intent.getDoubleExtra(MainActivity.EXTRA_TAXRATE3, 1.1);
+
+        et_LowerBracketTax = (EditText)findViewById(R.id.et_LowerTaxBracket);
+        et_MiddleBracketTax = (EditText)findViewById(R.id.et_MiddleTaxBracket);
+
+        et_LowerBracketRate = (EditText)findViewById(R.id.et_LowerBracketRate);
+        et_MiddleBracketRate = (EditText)findViewById(R.id.et_MiddleBracketRate);
+        et_UpperBracketRate = (EditText)findViewById(R.id.et_UpperBracketRate);
+
+        // Set current values
+        et_LowerBracketTax.setText("" + taxLevel1);
+        et_MiddleBracketTax.setText("" + taxLevel2);
+
+        et_LowerBracketRate.setText(String.format("%.2f", taxRate1 * 100));
+        et_MiddleBracketRate.setText(String.format("%.2f", taxRate2 * 100));
+        et_UpperBracketRate.setText(String.format("%.2f", taxRate3 * 100));
+
+        // Save button
+        Button btn_save = (Button)findViewById(R.id.btn_Save);
+        btn_save.setOnClickListener(saveSettings);
+    }
+
+    private final View.OnClickListener saveSettings =
+            new View.OnClickListener()
+            {
+                public void onClick(View view)
+                {
+                    taxLevel1 = Integer.parseInt(et_LowerBracketTax.getText().toString());
+                    taxLevel2 = Integer.parseInt(et_MiddleBracketTax.getText().toString());
+
+                    taxRate1 = Double.parseDouble(et_LowerBracketRate.getText().toString());
+                    taxRate2 = Double.parseDouble(et_MiddleBracketRate.getText().toString());
+                    taxRate3 = Double.parseDouble(et_UpperBracketRate.getText().toString());
+
+                    if (!checkParameters())
+                    {
+                        return;
+                    }
+
+                    Intent intent = getIntent();
+                    intent.putExtra(MainActivity.EXTRA_LOWERTAXBRACKET, taxLevel1);
+                    intent.putExtra(MainActivity.EXTRA_MIDDLETAXBRACKET, taxLevel2);
+                    intent.putExtra(MainActivity.EXTRA_TAXRATE1, taxRate1);
+                    intent.putExtra(MainActivity.EXTRA_TAXRATE2, taxRate2);
+                    intent.putExtra(MainActivity.EXTRA_TAXRATE3, taxRate3);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            };
+
+    private boolean checkParameters()
+    {
+        if (taxLevel1 > taxLevel2)
+            return false;
+
+        return true;
+    }
+}
