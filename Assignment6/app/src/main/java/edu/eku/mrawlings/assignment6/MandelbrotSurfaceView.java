0eb0cf8178ffe85ff63ca825278df2a7d8c50315
mrawlingst <mrawlingst@gmail.com>
2017-10-31 18:38:41 -0400

    [A6] Screw this

--- Assignment6/app/src/main/java/edu/eku/mrawlings/assignment6/MandelbrotSurfaceView.java
+++ Assignment6/app/src/main/java/edu/eku/mrawlings/assignment6/MandelbrotSurfaceView.java
@@ -3,6 +3,7 @@ package edu.eku.mrawlings.assignment6;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
@@ -16,11 +17,19 @@ public class MandelbrotSurfaceView extends SurfaceView implements SurfaceHolder.
     final int MESSAGE_QUIT = 0;
     final int MESSAGE_UPDATE = 1;
 
+    //
+    final int MAX_ITERATIONS = 500;
+
     Handler handler;
     SurfaceHolder holder;
     DrawingThread drawingThread;
 
+    // Size of view
     int viewSize = 0;
+
+    //
+    final int ZOOM = 150;
+    double zX, zY, cX, cY, temp;
 
     MandelbrotSurfaceView(Context context)
     {
@@ -87,6 +96,31 @@ public class MandelbrotSurfaceView extends SurfaceView implements SurfaceHolder.
             }
 
             canvas.drawColor(Color.GREEN);
+
+            System.out.println("MANDLEBROT");
+
+            for (int y = 0; y < viewSize; y++)
+            {
+                for (int x = 0; x < viewSize; x++)
+                {
+                    zX = 0;
+                    zY = 0;
+                    cX = (x - 400) / ZOOM;
+                    cY = (y - 300) / ZOOM;
+                    int iter = MAX_ITERATIONS;
+                    while ((zX * zX + zY * zY) < 4 && iter > 0)
+                    {
+                        temp = zX * zX - zY * zY + cX;
+                        zY = 2.0 * zX * zY + cY;
+                        zX = temp;
+                        iter--;
+                    }
+                    Paint paint = new Paint();
+                    paint.setColor(iter | (iter << 8));
+
+                    canvas.drawPoint(x, y, paint);
+                }
+            }
 
             holder.unlockCanvasAndPost(canvas);
             handler.sendEmptyMessageDelayed(MESSAGE_UPDATE, 100);
